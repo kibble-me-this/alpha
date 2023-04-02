13423685031acc5901a9866989b842e20b95eca7
Carlos Herrera <carlos@petastic.com>
2023-04-02 13:19:21 -0400

    added create pet form

--- src/pages/BlankPage.js
+++ src/pages/BlankPage.js
@@ -1,34 +1,28 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { alpha } from '@mui/material/styles';
-import { Container, Typography, Box } from '@mui/material';
+import { Container } from '@mui/material';
+// routes
+// import { PATH_DASHBOARD } from '../../routes/paths';
 // components
 // import { useSettingsContext } from '../../components/settings';
+// import CustomBreadcrumbs from '../../components/custom-breadcrumbs';
+// sections
+import UserNewEditForm from '../sections/@dashboard/user/UserNewEditForm';
 
 // ----------------------------------------------------------------------
 
-export default function BlankPage() {
+export default function UserCreatePage() {
   // const { themeStretch } = useSettingsContext();
 
   return (
     <>
       <Helmet>
-        <title> Blank Page | Minimal UI</title>
+        <title> User: Create a new user | Minimal UI</title>
       </Helmet>
 
       <Container>
-        <Typography variant="h4"> Blank </Typography>
 
-        <Box
-          sx={{
-            mt: 5,
-            width: 1,
-            height: 320,
-            borderRadius: 2,
-            bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
-            border: (theme) => `dashed 1px ${theme.palette.divider}`,
-          }}
-        />
+        <UserNewEditForm />
       </Container>
     </>
   );
